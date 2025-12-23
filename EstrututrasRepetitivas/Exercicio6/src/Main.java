332fd119bf9deb53d20168c410b16d318658f37e
Pedro Henrique Carneiro da Silva <phpedrocarneiro@gmail.com>
2025-12-23 20:38:58 -0300

    Add files via upload

---
+++ EstrututrasRepetitivas/Exercicio6/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Read an integer N and calculate all its divisors.\n");
+
+        System.out.println("Enter value: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if(n % i == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
